8ab7c3302c263c5adfd697980b76bd3f9425ffd2
Shiv1202 <shivansht9211@gmail.com>
2019-12-30 05:05:59 +0530

    single_linked_list_insertion.py file added in the Linked_list directory.

--- Linked _List/single_linked_list/single_linked_list_insertion.py
+++ Linked _List/single_linked_list/single_linked_list_insertion.py
@@ -1,12 +1,10 @@
 # Implementation of Single Linked List.
 """*************************************
-NOTE: (Linked List Creation and Traversal.)
-1) linked list is represented by a pointer
-to the first node of the list.
-2) Each node in a list consist at least
-two parts:
-    a) Data part.
-    b) Pointer (or reference) to the next node.
+NOTE: (Linked List Insertion.)
+Inserting a node in a list done by 3 ways.
+1) At the front of the linked list.
+2) After a given node.
+3) At the end of the linked list.
 ****************************************"""
 # Node Class 
 class Node:
@@ -22,6 +20,17 @@ class Linkedlist:
     list object."""
     def __init__(self):
         self.head = None
+
+        """ Function for inserting 
+        a node at the start of List."""
+    def push_start(self, new_data):
+        # Allocating new node with new_data.
+        new_node = Node(new_data)
+        # Make next of new Node as head.
+        new_node.next = self.head
+        # Move head to point to new node.
+        self.head = new_node
+
         """ Function for printing the 
         contents of linked list."""
     def print_list(self):
@@ -29,3 +38,19 @@ class Linkedlist:
         while temp:
             print(temp.data, end = " ")
             temp = temp.next
+
+# Driver Code.
+if __name__ == "__main__":
+    # Start with empty linked list.
+    L_list = Linkedlist()
+    L_list.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+    # Now Link first node with second
+    L_list.head.next = second
+    # And second node with third
+    second.next = third
+    # Inserting New Node.
+    L_list.push_start(4)
+    # Print List
+    L_list.print_list()
