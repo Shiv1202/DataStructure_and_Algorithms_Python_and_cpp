24cc15f1b1c6d3181c6fc06bc7db94b5f732687d
Shiv1202 <shivansht9211@gmail.com>
2020-01-26 22:34:51 +0530

    Basic Methods file uploaded.

---
+++ String_methods.py
@@ -0,0 +1,20 @@
+""" Important String Methods """
+# 1) find("string", beg, end)
+""" It return "-1" if string is not found
+in given range.
+It return first occurrence of string if found."""
+s = "Hello everyone, Main String here."
+print(s.find("Main", 0))
+
+# 2) islower()
+""" This Function return "True" if all the 
+letters in the string are lower cased, 
+otherwise "False"."""
+# Example Code.
+s = "hello"
+print(s.islower())
+
+# 2) islower()
+""" This Function return "True" if all the 
+letters in the string are lower cased, 
+otherwise "False"."""
