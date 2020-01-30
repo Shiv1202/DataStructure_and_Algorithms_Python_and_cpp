80087e409e68e0a6a18557b0ffaa07e9dce2aa33
Shiv1202 <shivansht9211@gmail.com>
2020-01-30 22:23:04 +0530

    PPSUM.py file Updated.

--- CodeChef/PPSUM.py
+++ CodeChef/PPSUM.py
@@ -1,11 +1,13 @@
-def sum_D(D, s):
-    return 0
-
 for i in range(int(input())):
     D, N = map(int, input().split())
     s = 0
-    for i in range(N + 1):
-        s += i
+    if D == 1:
+        s += (N * (N + 1)) // 2
+    else:
+        while D >= 1:
+            s = 0
+            s += (N * (N + 1)) // 2
+            N = s
+            D -= 1
     print(s)
-    ans = sum_D(D - 1, s)
         
