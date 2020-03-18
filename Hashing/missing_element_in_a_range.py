aa6d5cfbb02a644fb82e83d9e68c99c239d6b610
Shiv1202 <shivansht9211@gmail.com>
2020-03-18 21:03:08 +0530

    Hashing Folder Uploaded.

---
+++ Hashing/missing_element_in_a_range.py
@@ -0,0 +1,27 @@
+""" *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+Given an array arr[0.. n - 1] of distinct ele.
+and a range [low, high], find all no that are 
+in range but not in array. The missing ele
+should be printed in sorted order.
+--------------> Example <-----------------
+Input: arr = [10, 12, 11, 15]
+    low = 10, high = 15
+Output: 13, 14
+*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* """
+
+# Missing Element Function.
+def missing_ele(arr, n, l, h):
+    s = set(arr)
+    for i in range(l, h + 1):
+        if i not in s:
+            print(i, end = " ")
+
+# Main Function.
+def main():
+    arr = [1, 3, 5, 4]
+    n = len(arr)
+    low, high = 1, 10
+    missing_ele(arr, n, low, high)
+
+if __name__ == "__main__":
+    main()
