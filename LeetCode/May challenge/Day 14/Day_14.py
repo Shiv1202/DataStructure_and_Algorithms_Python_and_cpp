4fe7f7db41fbbe783ea563f2896137c6337d6081
Shivansh Tiwari <shivansht9211@gmail.com>
2020-05-14 21:21:40 +0530

    Create Day_14.py

---
+++ LeetCode/May challenge/Day 14/Day_14.py
@@ -0,0 +1,51 @@
+class Node:
+    
+    def __init__(self):
+        self.edges = collections.defaultdict(Node)
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = Node()
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self.root
+        for c in word:
+            node = node.edges[c]
+        node = node.edges["$"]
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self.root
+        for c in word:
+            if c in node.edges:
+                node = node.edges[c]
+            else:
+                return False
+        return "$" in node.edges
+    
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.root
+        for c in prefix:
+            if c in node.edges:
+                node = node.edges[c]
+            else:
+                return False
+        return True
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
