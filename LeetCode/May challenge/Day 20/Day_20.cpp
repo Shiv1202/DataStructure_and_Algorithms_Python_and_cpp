3f01539952063eedf9422ad5540385db1bcd0652
Shreya-Gupta02 <shreyagupta72484@gmail.com>
2020-05-20 20:16:07 +0530

    Create Day_20.cpp

---
+++ LeetCode/May challenge/Day 20/Day_20.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> arr;
+    vector<int> inorder(TreeNode* root)
+    {
+        if(root==NULL)
+            return arr;
+ 
+        inorder(root->left);
+        arr.push_back(root->val);
+        inorder(root->right);
+        return arr;
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        inorder(root);
+        return arr[k-1];
+    }
+};
