496389ad12dad0fbb4d1b809d5dfbbed9db4901b
Shreya Gupta <shreyagupta72484@gmail.com>
2020-05-21 16:12:10 +0530

    Update Day_21.cpp

--- LeetCode/May challenge/Day 21/Day_21.cpp
+++ LeetCode/May challenge/Day 21/Day_21.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
     int countSquares(vector<vector<int>>& matrix) {
         int sum=0;
-        vector<vector<int>> dp( matrix.size()+1 , vector<int> (matrix[0].size()+1, 0));
-        
-        for(int i=1;i<=matrix.size();i++)
+        for(int i=0;i<matrix.size();i++)
         {
-            for(int j=1;j<=matrix[0].size();j++)
+            for(int j=0;j<matrix[0].size();j++)
             {
-                if(matrix[i-1][j-1]!=0)
-                    dp[i][j]=min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]})+1;
-                    
-                sum+=dp[i][j];
+                if(i && j && matrix[i][j]!=0)
+                    matrix[i][j]=min({matrix[i-1][j],matrix[i][j-1],matrix[i-1][j-1]})+1;
+                sum+=matrix[i][j];
             }
         }
         return sum;
