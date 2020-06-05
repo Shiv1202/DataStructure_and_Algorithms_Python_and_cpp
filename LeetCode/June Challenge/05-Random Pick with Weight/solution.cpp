df3f465a3a21faac473e30e7819dfbc139bab61f
Shreya Gupta <shreyagupta72484@gmail.com>
2020-06-05 18:28:25 +0530

    Update solution.cpp

--- LeetCode/June Challenge/05-Random Pick with Weight/solution.cpp
+++ LeetCode/June Challenge/05-Random Pick with Weight/solution.cpp
@@ -16,7 +16,36 @@ public:
     {
         int r= rand() % prefix_sum.back();
         return upper_bound(prefix_sum.begin(),prefix_sum.end(),r) - prefix_sum.begin();
+        //upper_bound() returns an iterator pointing to the first element in the range [first, last) that is greater than value, or last if no such element is found. 
     }
+    //using linear search
+    /*
+    int pickIndex() 
+    {
+        int i,r= rand() % prefix_sum.back();
+        for(i=0;i<prefix_sum.size();i++)
+        {
+            if(prefix_sum[i]>r)
+                break;
+        }
+        return i;
+    }
+    */
+
+    //using binary search
+    /*
+    int pickIndex() 
+    {
+        int left = 0, right = accu.size();
+        int tgt = rand() % sum;
+        while(left < right){
+            int mid = left + (right-left) / 2;
+            if(accu[mid] <= tgt) left = mid + 1;
+            else right = mid;
+        }
+        return right;
+    }
+    */
 };
 
 /**
