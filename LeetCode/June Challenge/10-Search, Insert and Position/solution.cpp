927aab2fb69e72188d9df306de963ab290905a23
Shreya Gupta <shreyagupta72484@gmail.com>
2020-06-11 19:57:05 +0530

    Update solution.cpp

--- LeetCode/June Challenge/10-Search, Insert and Position/solution.cpp
+++ LeetCode/June Challenge/10-Search, Insert and Position/solution.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-
-        return lower_bound (nums.begin(), nums.end(), target)-nums.begin();
+        int low=0,high=nums.size()-1;
+        
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(nums[mid]==target)
+                return mid;
+            if(nums[mid]<target)
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return low;
     }
 };
-
-/*The lower_bound() method in C++ is used to return an iterator pointing to the first element in the range [first, last) which has a 
-value not less than val. This means that the function returns the index of the next smallest number just greater than or equal to that
-number. If there are multiple values that are equal to val, lower_bound() returns the index of the first such value.*/
