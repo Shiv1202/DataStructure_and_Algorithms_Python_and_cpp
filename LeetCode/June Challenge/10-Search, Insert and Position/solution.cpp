9be995de3b0ced123fc7bff8bd76355390256294
Shreya Gupta <shreyagupta72484@gmail.com>
2020-06-11 19:49:51 +0530

    Update solution.cpp

--- LeetCode/June Challenge/10-Search, Insert and Position/solution.cpp
+++ LeetCode/June Challenge/10-Search, Insert and Position/solution.cpp
@@ -6,4 +6,6 @@ public:
     }
 };
 
-/*The lower_bound() method in C++ is used to return an iterator pointing to the first element in the range [first, last) which has a value not less than val. This means that the function returns the index of the next smallest number just greater than or equal to that number. If there are multiple values that are equal to val, lower_bound() returns the index of the first such value.*/
+/*The lower_bound() method in C++ is used to return an iterator pointing to the first element in the range [first, last) which has a 
+value not less than val. This means that the function returns the index of the next smallest number just greater than or equal to that
+number. If there are multiple values that are equal to val, lower_bound() returns the index of the first such value.*/
